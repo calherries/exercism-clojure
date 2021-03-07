a40df81c8d7a4308b9566341830fbd70bf6c52c7
Callum Herries <hi@callumherries.com>
2021-03-07 09:55:21 +0100

    Add previous-names check to robot-name

--- robot-name/src/robot_name.clj
+++ robot-name/src/robot_name.clj
@@ -3,16 +3,22 @@
 (defn random-char []
   (char (+ 65 (rand-int 26))))
 
-(defn new-random-name []
+(def previous-names
+  (atom []))
+
+(defn random-name []
   (apply str
-   (concat (repeatedly 2 random-char)
-           (repeatedly 3 #(rand-int 9)))))
+         (concat (repeatedly 2 random-char)
+                 (repeatedly 3 #(rand-int 9)))))
 
-(comment
-  (int \A)
-  (new-random-name))
+(defn new-random-name []
+  (let [name (->> (repeatedly 3 random-name)
+                  (filter #(not (.contains @previous-names %)))
+                  first)]
+    (swap! previous-names conj name)
+    name))
 
-(defn robot [] ;; <- arglist goes here
+(defn robot []
   (atom {:name (new-random-name)}))
 
 (defn robot-name [robot]
@@ -22,6 +28,9 @@
   (swap! robot assoc :name (new-random-name)))
 
 (comment
+  (random-name)
+  @previous-names
+  (new-random-name)
   (def x (robot))
   (robot-name x)
   (reset-name x))
