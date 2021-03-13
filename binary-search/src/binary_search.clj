048c3d738571624faf9467e3ce48d0c27fdcc99c
Callum Herries <hi@callumherries.com>
2021-03-13 11:08:21 +0100

    binary-search

---
+++ binary-search/src/binary_search.clj
@@ -0,0 +1,16 @@
+(ns binary-search)
+
+(defn middle [lst]
+  (quot (- (count lst) 1) 2))
+
+(defn search-for [item lst]
+  (loop [remaining lst
+                left-idx 0]
+    (if (empty? remaining)
+      (throw (ex-info "not found" {})))
+    (let [mid (middle remaining)
+          mid-item (nth remaining mid)]
+      (case (compare item mid-item)
+        0 (+ mid left-idx)
+        -1 (recur (take mid remaining) left-idx)
+        1 (recur (drop (inc mid) remaining) (+ 1 mid left-idx))))))
