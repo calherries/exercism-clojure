399e8ddf57d3d319ac4b1fb34625f33f26448c40
Callum Herries <hi@callumherries.com>
2021-04-15 11:46:17 +0200

    Update pascals-triangle

--- pascals-triangle/src/pascals_triangle.clj
+++ pascals-triangle/src/pascals_triangle.clj
@@ -1,23 +1,17 @@
 (ns pascals-triangle)
 
+
+(defn row-from-previous [previous]
+  (cons 1 (->> (partition-all 2 1 previous)
+               (map (partial apply +)))))
+
+(comment
+  (map row-from-previous [[1] [1 1] [1 2 1]]))
+
 (def triangle
   (lazy-seq
    (cons [1]
-         )))
+         (map row-from-previous triangle))))
 
-(defn row-from-previous [previous]
-  (loop [acc [1]
-         remaining previous]
-    (if (empty? remaining)
-      acc
-      (if (empty? (rest remaining))
-        (conj acc 1)
-        (recur (conj acc (+ (first previous) (second previous)))
-               (rest previous))))))
-
-(map row-from-previous [[1] [1 1] [1 2 1]])
-
-(row-from-previous [1])
-
-(defn row [] ;; <- arglist goes here
-)
+(defn row [n]
+  (nth triangle (dec n)))
